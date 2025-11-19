734ccac1f81b0e793d1e00b6124376f672964fc1
naantuam <nathaniellongmen@gmail.com>
2025-11-19 16:54:16 +0100

    Update ..

--- src/UserDetail/AuthComponents/AuthCard.jsx
+++ src/UserDetail/AuthComponents/AuthCard.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const AuthCard = ({ children }) => {
   return (
     <div className="bg-black/50 md:bg-white/10 backdrop-blur-xl rounded-2xl border border-white/20 shadow-xl 
-     p-5 w-full max-w-md min-h-[90vh] max-h-[96vh] overflow-y-auto md:min-h-fit">
+     p-5 w-full max-w-md min-h-[93vh] max-h-[96vh] overflow-y-auto md:min-h-fit">
       {children}
     </div>
   );
